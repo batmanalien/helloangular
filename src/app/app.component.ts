7995ccebf01c2c90ffdcf6f699a7e07e8bed3300
Mason Liang <gliang1984@gmail.com>
2017-08-11 09:50:32 -0600

    Added IE support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mason\'s Wonderful World with Angular!';
 }
